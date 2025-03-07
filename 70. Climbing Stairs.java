b40087573b79d23290f67636c68b80bef0944298
Yashwant Thakare <142816930+Yash-thakare@users.noreply.github.com>
2025-03-07 10:00:05 +0530

    Add files via upload

---
+++ 70. Climbing Stairs.java
@@ -0,0 +1,22 @@
+class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n == 1) return 1; 
+        if (n == 2) return 2; 
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1; 
+        dp[2] = 2; 
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2]; 
+        }
+
+        return dp[n]; 
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs cs = new ClimbingStairs();
+        System.out.println(cs.climbStairs(2)); // Output: 2
+        System.out.println(cs.climbStairs(3)); // Output: 3
+    }
+}
