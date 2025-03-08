9480ab2b54c727a6da7acb0ad206370649e34b14
Yashwant Thakare <142816930+Yash-thakare@users.noreply.github.com>
2025-03-08 10:44:47 +0530

    Add files via upload

---
+++ WordCount.java
@@ -0,0 +1,9 @@
+class wordCount{
+    public static void main(String[] args) {
+     String str ="Yash";
+     String[] w =str.split("");
+     int wordCount =w.length;
+     System.out.println("word count " + wordCount);
+  
+    }
+}
