49599c082a765f6d3dbafe59375e2494b3723bd7
Yashwant Thakare <142816930+Yash-thakare@users.noreply.github.com>
2025-04-02 09:15:09 +0530

    Add files via upload

---
+++ Defanging_ip_address .java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner (System.in);
+        System.out.println(sc.nextLine().replace(".","[.]"));
+    }
+}
+
+//input = 1.1.1.1
+//output = 1[.]1[.]1[.]1
